90342286012b4cd3eba42eb53a8d7e5f14f431ae
Timothy John <tim.gandionco@gmail.com>
2017-10-15 21:20:45 +0800

    android ios build

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,11 +1,9 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core';
 import { KeyboardComponent } from './keyboard/keyboard';
-import { IntroSliderComponent } from './intro-slider/intro-slider';
 @NgModule({
 	declarations: [KeyboardComponent],
 	imports: [CommonModule],
-	exports: [KeyboardComponent,
-    IntroSliderComponent]
+	exports: [KeyboardComponent]
 })
 export class ComponentsModule {}
